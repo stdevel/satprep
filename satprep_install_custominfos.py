8eb5c90719f3a2a9f13a4ef39c40e90dee761d2d
Niko Wenselowski <der@nik0.de>
2014-09-12 12:36:55 +0200

    Using that new exception instead of just sys.exit-ing

--- satprep_install_custominfos.py
+++ satprep_install_custominfos.py
@@ -14,7 +14,7 @@ import pprint
 import sys
 import xmlrpclib
 from optparse import OptionParser
-from satprep_shared import SUPPORTED_API_LEVELS, get_credentials
+from satprep_shared import APILevelNotSupportedException, SUPPORTED_API_LEVELS, get_credentials
 # TODO: state prod/test
 
 CUSTOM_KEYS = {
@@ -52,8 +52,10 @@ def main(options):
 def check_if_api_is_supported(client):
 	api_level = client.api.getVersion()
 	if api_level not in SUPPORTED_API_LEVELS:
-		LOGGER.warning("INFO: your API version (" + api_level + ") does not support the required calls. You'll need API version 1.8 (11.1) or higher!")
-		sys.exit(1)
+		raise APILevelNotSupportedException(
+			"Your API version ({0}) does not support the required calls. "
+			"You'll need API version 1.8 (11.1) or higher!".format(api_level)
+		)
 	else:
 		LOGGER.info("INFO: supported API version (" + api_level + ") found.")
 
--- satprep_snapshot.py
+++ satprep_snapshot.py
@@ -15,7 +15,7 @@ import os
 import time
 import xmlrpclib
 from optparse import OptionParser
-from satprep_shared import SUPPORTED_API_LEVELS, get_credentials
+from satprep_shared import APILevelNotSupportedException, SUPPORTED_API_LEVELS, get_credentials
 
 #TODO: string + " " + string ==>  string,string
 #TODO: escaping ==> r'\tbla}t'
@@ -99,13 +99,12 @@ def main(options):
 	#check whether the API version matches the minimum required
 	api_level = client.api.getVersion()
 	if api_level not in SUPPORTED_API_LEVELS:
-		LOGGER.critical(
+		raise APILevelNotSupportedException(
 			"ERROR: your API version ({0}) does not support the required "
 			"calls. You'll need API version 1.8 (11.1) or higher!".format(
 				api_level
 			)
 		)
-		exit(1)
 	else:
 		LOGGER.info("supported API version ({0}) found.".format(api_level))
 
